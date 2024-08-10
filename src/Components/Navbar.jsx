cb37444bece558edfe531f1b2f561cc34bb99a45
Maxzi3 <obisikemaxwell@gmail.com>
2024-08-10 20:03:24 +0100

    navbar update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,8 +10,8 @@ const Navbar = () => {
   };
   const linkclass = ({ isActive }) =>
     isActive
-      ? " mr-5 hover:text-orange-800 overline"
-      : "mr-5 hover:text-orange-800 ";
+      ? " mr-5 hover:text-orange-800 px-3 py-2 overline"
+      : "mr-5 hover:text-orange-800 px-3 py-2  ";
   return (
     <div>
       <header className="fixed top-0 left-0 right-0 z-50 text-white body-font w-full bg-orange-600">
@@ -21,7 +21,10 @@ const Navbar = () => {
             className="flex flex-col  text-white font-medium items-center ml-3 text-sm  hover:text-orange-800  hover:bg-white"
           >
             <span className="text-xl">IKW</span>{" "}
-            <span className="border-t-2  border-orange-800 hover:border-white p-1"> VENTURES</span>
+            <span className="border-t-2  border-orange-800 hover:border-white p-1">
+              {" "}
+              VENTURES
+            </span>
           </NavLink>
           <div className="md:hidden">
             <button onClick={toggleMenu}>
@@ -32,37 +35,42 @@ const Navbar = () => {
               )}
             </button>
           </div>
+          {/* Mobile view for Menu  */}
+          <nav
+            className={`${
+              isOpen ? "max-h-40 opacity-100" : "max-h-0 opacity-0"
+            } overflow-hidden transition-all duration-300 ease-in-out md:hidden flex flex-col items-end text-base w-full md:w-auto`}
+          >
+            <NavLink to="/" className={linkclass} onClick={toggleMenu}>
+              Home
+            </NavLink>
+            <NavLink to="/About" className={linkclass} onClick={toggleMenu}>
+              About Us
+            </NavLink>
+            <NavLink to="/Product" className={linkclass} onClick={toggleMenu}>
+              Products
+            </NavLink>
+            <NavLink to="/Contact" className={linkclass} onClick={toggleMenu}>
+              Contact
+            </NavLink>
+          </nav>
+
+          {/* Desktop menu links */}
           <nav
             className={`${
               isOpen ? "block" : "hidden"
-            } md:flex flex-col md:flex-row items-center text-base justify-center w-full md:w-auto`}
+            } hidden md:flex flex-col md:flex-row items-center text-base justify-center w-full md:w-auto`}
           >
-            <NavLink
-              to="/"
-              className={linkclass}
-              onClick={toggleMenu}
-            >
+            <NavLink to="/" className={linkclass} onClick={toggleMenu}>
               Home
             </NavLink>
-            <NavLink
-              to="/About"
-              className={linkclass}
-              onClick={toggleMenu}
-            >
+            <NavLink to="/About" className={linkclass} onClick={toggleMenu}>
               About Us
             </NavLink>
-            <NavLink
-              to="/Product"
-              className={linkclass}
-              onClick={toggleMenu}
-            >
+            <NavLink to="/Product" className={linkclass} onClick={toggleMenu}>
               Products
             </NavLink>
-            <NavLink
-              to="/Contact"
-              className={linkclass}
-              onClick={toggleMenu}
-            >
+            <NavLink to="/Contact" className={linkclass} onClick={toggleMenu}>
               Contact
             </NavLink>
           </nav>
